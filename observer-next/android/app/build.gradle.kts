195fa6ec318cac5ab8465e0521899f43d04f74fb
Djaeger1 <79890209+Djaeger1@users.noreply.github.com>
2026-09-22 08:21:21 +0700

    Bump adaptive APK to 1.1.3 runtimefix

--- observer-next/android/app/build.gradle.kts
+++ observer-next/android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.djaeger.observer"
         minSdk = 26
         targetSdk = 33
-        versionCode = 106
-        versionName = "1.1.2-credrestore"
+        versionCode = 107
+        versionName = "1.1.3-runtimefix"
     }
 
     buildFeatures {
