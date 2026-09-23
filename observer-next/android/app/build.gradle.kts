445bec3c2ca569fc1f621823575d7a7a187a03f0
Djaeger1 <79890209+Djaeger1@users.noreply.github.com>
2026-09-23 21:10:13 +0700

    release: set DJAEGER GAMING app identity v1.1.8 vc112

--- observer-next/android/app/build.gradle.kts
+++ observer-next/android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.djaeger.observer"
         minSdk = 26
         targetSdk = 33
-        versionCode = 111
-        versionName = "1.1.7-adaptivefix"
+        versionCode = 112
+        versionName = "1.1.8-v3.4-final"
     }
 
     buildFeatures {
